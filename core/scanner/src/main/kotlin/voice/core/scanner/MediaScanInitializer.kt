1a2c8131fa92467956848f8a1f4f5000d357b853
Nosson Savin <106287322+NossonSavin@users.noreply.github.com>
2026-08-30 20:09:04 +0300

    Only trigger automatic scan right after selecting a folder, removing unsolicited scans on app startup and view attach

--- core/scanner/src/main/kotlin/voice/core/scanner/MediaScanInitializer.kt
+++ core/scanner/src/main/kotlin/voice/core/scanner/MediaScanInitializer.kt
@@ -8,6 +8,5 @@ import voice.core.initializer.AppInitializer
 public class MediaScanInitializer(private val mediaScanTrigger: MediaScanTrigger) : AppInitializer {
 
   override fun onAppStart(application: android.app.Application) {
-    mediaScanTrigger.triggerScan()
   }
 }
